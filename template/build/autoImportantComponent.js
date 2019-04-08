d9ffac5e9a89116bf9dd4517402dd37a49c5f48c
hkaikai <617760820@qq.com>
2019-04-08 22:44:17 +0800

    Update autoImportantComponent.js

--- template/build/autoImportantComponent.js
+++ template/build/autoImportantComponent.js
@@ -1,17 +1,33 @@
 var path = require('path')
+var fs = require('fs')
 var config = require('../config')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
 const filePath = path.resolve(__dirname, '../src/router/index.js')
 const file = require(filePath)
 
 function ImportantComponent() {
+
   let results = [],
     plugins = []
   file.pages.forEach(page => {
     if (!page.config || !page.config.usingComponents) return
-    for (let ket in page.config.usingComponents) {
-      ket.split('-')[0] === 'wux' && results.push(ket.split('-')[1])
+    const getC = function(usingComponents) {
+      for (let key in usingComponents) {
+        if (key.split('-')[0] !== 'wux') continue
+        const c = key.split('-')[1]
+        results.push(c)
+        try {
+          const f = path.resolve(__dirname, '../node_modules/wux-weapp/dist/' + c + '/index.json')
+          let date = fs.readFileSync(f, 'utf-8')
+          date = JSON.parse(date)
+          if (!date.usingComponents) continue
+          getC(date.usingComponents)
+        } catch (e) {
+          console.log(chalk.yellow('组件依赖引入可能失败'))
+        }
+      } 
     }
+    getC(page.config.usingComponents)
   })
   results = [...new Set(results)]
   results.forEach(c => {
@@ -26,5 +42,7 @@ function ImportantComponent() {
   }]))
   return plugins
 }
+
+ImportantComponent()
 
 module.exports = ImportantComponent
